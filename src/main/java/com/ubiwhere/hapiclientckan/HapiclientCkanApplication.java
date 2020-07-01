12c3447ca3987992775740d66521c5aa8c7aa037
jpimentel <jpimentel@ubiwhere.com>
2020-07-01 11:35:07 +0100

    Initial commit

---
+++ src/main/java/com/ubiwhere/hapiclientckan/HapiclientCkanApplication.java
@@ -0,0 +1,13 @@
+package com.ubiwhere.hapiclientckan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapiclientCkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapiclientCkanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubiwhere/hapiclientckan/HapiclientCkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubiwhere.hapiclientckan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapiclientCkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
